c2a6abee8aeeba049b5f7d8727bcb3fee5cedc22
virustotalop <virustotalop@gmail.com>
2021-10-06 20:50:58 -0700

    Fix upstream script

--- updateUpstream.sh
+++ updateUpstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote add upstream https://github.com/tebexio/BuycraftX/Geyser.git
+git remote add upstream https://github.com/tebexio/BuycraftX.git
 git fetch upstream
 git checkout master
 git merge upstream/master
